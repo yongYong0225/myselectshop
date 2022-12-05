10d71b5019708dd496f7d4099e3a2e96a96255a7
yongYong0225 <118151866+yongYong0225@users.noreply.github.com>
2022-12-06 00:02:47 +0900

    페이지 정렬 및 폴더생성

--- myselectshop/src/main/java/com/sparta/myselectshop/controller/ProductController.java
+++ myselectshop/src/main/java/com/sparta/myselectshop/controller/ProductController.java
@@ -3,12 +3,13 @@ package com.sparta.myselectshop.controller;
 import com.sparta.myselectshop.dto.ProductMypriceRequestDto;
 import com.sparta.myselectshop.dto.ProductRequestDto;
 import com.sparta.myselectshop.dto.ProductResponseDto;
+import com.sparta.myselectshop.entity.Product;
 import com.sparta.myselectshop.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -23,20 +24,19 @@ public class ProductController {
         // 응답 보내기
         return productService.createProduct(requestDto, request);
     }
+
     // 관심 상품 조회하기
     @GetMapping("/products")
-    public List<ProductResponseDto> getProducts(HttpServletRequest request) {
+    public Page<Product> getProducts(
+            @RequestParam("page") int page,
+            @RequestParam("size") int size,
+            @RequestParam("sortBy") String sortBy,
+            @RequestParam("isAsc") boolean isAsc,
+            HttpServletRequest request
+    ) {
         // 응답 보내기
-        return productService.getProducts(request);
+        return productService.getProducts(request, page-1, size, sortBy, isAsc);
     }
-
-
-//    // 관심 상품 조회하기
-//    @GetMapping("/products")
-//    public List<ProductResponseDto> getProducts() {
-//        // 응답 보내기
-//        return productService.getProducts();
-//    }
 
     // 관심 상품 최저가 등록하기
     @PutMapping("/products/{id}")
@@ -45,11 +45,15 @@ public class ProductController {
         return productService.updateProduct(id, requestDto, request);
     }
 
-//    // 관심 상품 최저가 등록하기
-//    @PutMapping("/products/{id}")
-//    public Long updateProduct(@PathVariable Long id, @RequestBody ProductMypriceRequestDto requestDto) {
-//        // 응답 보내기 (업데이트된 상품 id)
-//        return productService.updateProduct(id, requestDto);
-//    }
+    // 상품에 폴더 추가
+    @PostMapping("/products/{productId}/folder")
+    public Long addFolder(
+            @PathVariable Long productId,
+            @RequestParam Long folderId,
+            HttpServletRequest request
+    ) {
+        Product product = productService.addFolder(productId, folderId, request);
+        return product.getId();
+    }
 
 }
